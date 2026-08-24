4a3416e0259a5265f9da705f779221011cdcd3b1
Richard Beare <32406942+richdrummer33@users.noreply.github.com>
2026-08-24 08:53:35 -0400

    Expose coarse bank-three renderer entry points

--- src/gg_vfx.h
+++ src/gg_vfx.h
@@ -6,11 +6,20 @@
 #include "vfx_queue.h"
 #include "gg_world.h"
 
+#if defined(__SDCC)
+#include <gbdk/platform.h>
+#else
+#ifndef BANKED
+#define BANKED
+#endif
+#endif
+
 #define GG_VFX_SCRATCH_BASE 64u
 #define GG_VFX_SCRATCH_COUNT 32u
 #define GG_VFX_SPRITE_INDEX_BASE 16u
 #define GG_VFX_SPRITE_INDEX_END 40u
 #define GG_VFX_RASTER_BANDS 24u
+#define GG_VFX_GLOW_DIRTY_CAP 32u
 
 typedef struct {
     VfxQueue queue;
@@ -21,7 +30,8 @@ typedef struct {
     uint8_t beam_by[VFX_BEAM_MAX_CELLS];
     uint8_t glow_ref[GG_WORLD_BLOCK_W * GG_WORLD_BLOCK_H];
     uint8_t glow_shown[GG_WORLD_BLOCK_W * GG_WORLD_BLOCK_H];
-    uint16_t glow_dirty[32u];
+    uint8_t glow_dirty_bx[GG_VFX_GLOW_DIRTY_CAP];
+    uint8_t glow_dirty_by[GG_VFX_GLOW_DIRTY_CAP];
     uint8_t glow_dirty_count;
 
     uint8_t projectile_cache_valid[VFX_PROJECTILE_MAX];
@@ -30,7 +40,6 @@ typedef struct {
     uint8_t projectile_cache_seed[VFX_PROJECTILE_MAX];
 
     uint8_t ring_cache_radius;
-    uint8_t ring_cache_phase;
     uint8_t ring_cache_valid;
     uint8_t last_exposure;
     uint8_t last_blackout;
@@ -42,11 +51,18 @@ typedef struct {
     int8_t raster_offsets[GG_VFX_RASTER_BANDS];
 } GgVfx;
 
+/* Near/local bank-3 API. */
 void gg_vfx_init(GgVfx *fx);
 void gg_vfx_reset(GgVfx *fx, const Sim *sim);
 void gg_vfx_sync(GgVfx *fx, const VfxLab *lab, const Sim *sim);
 void gg_vfx_flush(GgVfx *fx, uint16_t budget_bytes);
 void gg_vfx_render_sprites(GgVfx *fx, const VfxLab *lab, uint8_t camera_x);
 void gg_vfx_build_raster_offsets(GgVfx *fx, const VfxLab *lab);
+
+/* Fixed-bank target entry points. Keep crossings coarse: one init call and one
+   render/transfer call per video frame. */
+void gg_vfx_target_init(GgVfx *fx, const Sim *sim) BANKED;
+void gg_vfx_target_frame(GgVfx *fx, const VfxLab *lab, const Sim *sim,
+                         uint8_t camera_x) BANKED;
 
 #endif
